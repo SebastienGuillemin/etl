83a079161985cb7c14044e306786a9a252bb7de7
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2023-12-05 14:07:23 +0100

    Prise en compte du dosage des PC

--- src/main/java/com/sebastienguillemin/stups/model/entity/resource/ProduitCoupage.java
+++ src/main/java/com/sebastienguillemin/stups/model/entity/resource/ProduitCoupage.java
@@ -8,6 +8,7 @@ import com.sebastienguillemin.stups.model.ResourceEntity;
 import com.sebastienguillemin.stups.model.entity.base.Composition;
 import com.sebastienguillemin.stups.repository.RDFRepository;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -30,13 +31,25 @@ public class ProduitCoupage extends ResourceEntity {
     @JoinColumn(name = "id_substance")
     private Substance substance;
 
+    @Column(name = "taux")
+    private String dosage;
+
+    private boolean trace;
+
+    public float getDosage() {
+        // TODO : que faire si pas de dosage et pas de en trace ?
+        return (this.dosage != null) ? Float.valueOf(this.dosage) : (this.trace) ? 0 : 0;
+    }
+
     @Override
     public Resource getResource(Model model) {
         Resource resource = model.createResource(RDFRepository.PREFIX + this.getResourceName());
         Property aSubstance = model.createProperty(RDFRepository.PREFIX + "aSubstance");
+        Property dosage = model.createProperty(RDFRepository.PREFIX + "dosage");
 
         resource.addProperty(aSubstance, this.substance.getResource(model));
-                
+        resource.addLiteral(dosage, this.getDosage());
+
         return resource;
     }    
 }
