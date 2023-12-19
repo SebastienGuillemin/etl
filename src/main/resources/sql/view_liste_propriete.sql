88a2ce26ab929cbd49e7caf0bc0bbe8968df1fc6
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2023-12-19 11:29:51 +0100

    Maj view

--- src/main/resources/sql/view_liste_propriete.sql
+++ src/main/resources/sql/view_liste_propriete.sql
@@ -1,9 +1,13 @@
 CREATE MATERIALIZED VIEW liste_propriete AS (
     (
-        SELECT -- Valeur de la propriété dans la table "valeur_propriete".
+        SELECT
+            -- Valeur de la propriété dans la table "valeur_propriete".
             e.id,
-            c.id AS id_composition,
-            p.libelle AS propriete,
+            CASE p.libelle
+                WHEN 'Couleur extérieur (comprimé)' THEN 'Couleur extérieur'
+                WHEN 'Couleur intérieur (comprimé)' THEN 'Couleur intérieur'
+                ELSE p.libelle
+            END AS propriete,
             vp.libelle AS valeur
         FROM
             echantillon e,
@@ -19,13 +23,16 @@ CREATE MATERIALIZED VIEW liste_propriete AS (
     )
     UNION
     (
-        SELECT -- Valeur de la propriété dans la table "description".
+        SELECT
+            -- Valeur de la propriété dans la table "description".
             e.id,
-            c.id AS id_composition,
             CASE p.libelle
                 WHEN 'Masse (résine)' THEN 'Masse'
                 WHEN 'Masse (comprimé)' THEN 'Masse'
-                ELSE P.libelle
+                WHEN 'Autre (résine)' THEN 'Autre'
+                WHEN 'Couleur extérieur (comprimé)' THEN 'Couleur extérieur'
+                WHEN 'Couleur intérieur (comprimé)' THEN 'Couleur intérieur'
+                ELSE p.libelle
             END AS propriete,
             d.valeur AS valeur
         FROM
@@ -37,13 +44,13 @@ CREATE MATERIALIZED VIEW liste_propriete AS (
             e.id_composition = c.id
             AND c.id = d.id_composition
             AND p.id = d.id_propriete
-            AND d.valeur is not null
+            AND d.valeur IS NOT NULL
     )
     UNION
     (
-        SELECT -- Récupération du type de drogue.
+        SELECT
+            -- Récupération du type de drogue.
             e.id,
-            c.id AS id_composition,
             'Type drogue' AS propriete,
             t.libelle AS valeur
         FROM
@@ -56,13 +63,13 @@ CREATE MATERIALIZED VIEW liste_propriete AS (
             e.id_composition = c.id
             AND pa.id_composition = c.id
             AND pa.id_substance = s.id
-            AND t.id = s.id_categorie
+            AND t.id = s.id_categORie
     )
     UNION
     (
-        SELECT -- Récupération du numéro d'échantillon.
+        SELECT
+            -- Récupération du numéro d'échantillon.
             e.id,
-            c.id AS id_composition,
             'Numero echantillon' AS propriete,
             e.num_echantillon AS valeur
         FROM
@@ -70,5 +77,39 @@ CREATE MATERIALIZED VIEW liste_propriete AS (
             composition c
         WHERE
             e.id_composition = c.id
+    )
+    UNION
+    (
+        SELECT
+            -- Récupération constituants à taux non nuls
+            e.id,
+            s.libelle AS "Propriete",
+            CAST(pa.taux as varchar) AS "Valeur"
+        FROM
+            echantillon e
+            INNER JOIN composition c ON c.id = e.id_composition
+            INNER JOIN principe_actif pa ON pa.id_composition = c.id
+            INNER JOIN substance s ON s.id = pa.id_substance
+        WHERE
+            taux IS NOT NULL
+    )
+    UNION
+    (
+        SELECT
+            -- Récupération constituants présents en trace (i.e. avec un taux nul)
+            e.id,
+            s.libelle AS "Propriete",
+            '0' AS "Valeur"
+        FROM
+            echantillon e
+            INNER JOIN composition c ON c.id = e.id_composition
+            INNER JOIN principe_actif pa ON pa.id_composition = c.id
+            INNER JOIN substance s ON s.id = pa.id_substance
+        WHERE
+            trace
+            AND (
+                taux IS NULL
+                OR taux = 0
+            )
     )
 )
