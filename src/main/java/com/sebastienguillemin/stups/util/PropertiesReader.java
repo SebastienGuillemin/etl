68c407ecf7add41ec6446e161b56185b475ba7ef
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2024-08-21 09:30:31 +0200

    Update properties reader

--- src/main/java/com/sebastienguillemin/stups/util/PropertiesReader.java
+++ src/main/java/com/sebastienguillemin/stups/util/PropertiesReader.java
@@ -1,7 +1,7 @@
 package com.sebastienguillemin.stups.util;
 
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.yaml.snakeyaml.Yaml;
@@ -16,20 +16,40 @@ public class PropertiesReader {
         return propertiesReader;
     }
 
-    private HashMap<String, String> properties;
+    private Map<String, Object> properties;
 
     public String getPropertyValue(String propertyName) {
         if (this.properties == null)
             this.loadProperties();
 
-        return Optional.of(this.properties.get(propertyName)).orElse("");
+        String parts[] = propertyName.split("\\.");
+
+        String value = "";
+        Map<String, Object> nextNode = this.properties;
+        Object next;
+        for (int i = 0; i < parts.length; i ++) {
+            next = nextNode.get(parts[i]);
+            if (next == null) {
+                return "";
+            }
+            else if (next instanceof Map)
+                nextNode = (Map<String, Object>) next;
+            else if (next instanceof String) {
+                if (i < parts.length - 1)
+                    return "";
+    
+                value = (String) next;
+            }
+        }
+
+        return value;
     }
 
     private void loadProperties() {
         Yaml yaml = new Yaml();
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("properties.yml");
         this.properties = yaml.load(inputStream);
-
+        
         System.out.println("Properties loaded.");
     }
 }
